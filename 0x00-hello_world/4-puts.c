fda1b242aed7980041b197857a6b82d89bbdeae5
BeccaQ <beccamiwa@gmail.com>
2022-04-08 00:30:14 +0100

    Fix: 4-puts.c remove the new line function and modified the coment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * prints "Programming is like building a multilingual puzzle.
- * Return 0 at the end.
+ * Main: prints the string 
+ * "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, otherwise non-zero
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
