a3ede200cd3d9de11663b0ef32d18ab01f17eab7
BeccaQ <beccamiwa@gmail.com>
2022-04-08 00:37:47 +0100

    fix 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * Main: prints the string 
- * "Programming is like building a multilingual puzzle
+ * main - prints the string "Programming is like building a multilingual puzzle
  * Return: 0 if exited properly, otherwise non-zero
  */
 
