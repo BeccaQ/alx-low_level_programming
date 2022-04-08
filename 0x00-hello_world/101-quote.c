5684ca4c6efff0f357c5131eb27fbb3332b867fd
BeccaQ <beccamiwa@gmail.com>
2022-04-08 12:15:01 +0100

    fix: 101-quote.c yet again ommitted #include <unistd.h> preprocessor at the begining

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - prints sizes of various types
  * Return: 1 if properly exited, else non-zero
  */
-
 int main(void)
 {
 	write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
