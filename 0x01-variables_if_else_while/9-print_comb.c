968ffa7255b2601e13c73de8a43446992584f3ac
BeccaQ <beccamiwa@gmail.com>
2022-04-10 14:51:49 -0700

    new commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,8 +5,7 @@
  * main -  print the alphabet in lowercase,
  * followed by a new line.
  * Return: 0 always if successful.
- */
-
+*/
 int main(void)
 {
 	int l = '0';
@@ -14,7 +13,7 @@ int main(void)
 	for (; l <= '9'; l++)
 	{
 		putchar(l);
-		if (l != '9')
+	if (l != '9')
 			putchar(',');
 			putchar(' ');
 	}
