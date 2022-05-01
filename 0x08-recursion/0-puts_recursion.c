5edb7281fbd9772f45c57781e60f4bd7bb935db8
BeccaQ <beccamiwa@gmail.com>
2022-05-01 23:10:49 +0100

    fixe 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,9 +11,9 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
-		_putchar();
+		return ('\n');
+		return ();
 	}
-	_putchar(*s);
+	return (*s);
 	_puts_recursion(s + 1);
 }
