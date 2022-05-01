857cef08e87ce3809cb83b2fc6bca045f22ceb43
BeccaQ <beccamiwa@gmail.com>
2022-05-02 00:04:50 +0100

    fixed 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,7 +9,6 @@
 
 void _puts_recursion(char *s)
 {
-<<<<<<< HEAD
 	int i = 0;
 
 	if (s[i] == '\0')
@@ -18,13 +17,4 @@ void _puts_recursion(char *s)
 	}
 	_putchar(*s);
 	_puts_recursion(s+1);
-=======
-	if (*s == '\0')
-	{
-		return ('\n');
-		return ();
-	}
-	return (*s);
-	_puts_recursion(s + 1);
->>>>>>> 995010cc170b15de0fb6b56fc97364ffed6cb547
 }
