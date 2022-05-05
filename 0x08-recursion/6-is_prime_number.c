6a6de40cf0c5e5450ce1f200d4217031318afd73
BeccaQ <beccamiwa@gmail.com>
2022-05-05 22:43:37 +0100

    fixed betty style in 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,11 +8,10 @@
  * @y: multiple
  * Return: a number
  */
-
 int _prime_num(int x, int y)
 {
-	if (y <=1 || (x != y && y % x == 0))
-	{ 
+	if (y <= 1 || (x != y && y % x == 0))
+	{
 		return (0);
 	}
 	else if (x == y)
@@ -24,7 +23,7 @@ int _prime_num(int x, int y)
 
 /**
  * is_prime_number - tells whether a number
- * is prime number or not 
+ * is prime number or not
  * @n: an integer
  * Return: a number
  */
