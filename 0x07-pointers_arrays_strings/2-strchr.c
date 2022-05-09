54ebdd79a8d184d8d1ea679bdffe49ee9c6b3715
BeccaQ <beccamiwa@gmail.com>
2022-05-09 13:30:10 +0100

    added more files to 0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,13 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
  * _strchr - locate a character in a string
  * @s:string
  * @c:char
  * Return:pointer to char*
  */
+
 char *_strchr(char *s, char c)
 {
 
