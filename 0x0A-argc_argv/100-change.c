677ea14acda611847fbaa12d1e1b66ccd045a8c9
BeccaQ <beccamiwa@gmail.com>
2022-05-11 22:20:34 +0100

    fixed segmentation error in 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,26 +15,26 @@ int main(int argc, char *argv[])
 	int i, num, num2, change = 0;
 	int coin[] = {25, 10, 5, 2, 1};
 
-	num = atoi(argv[1]);
-
-	if (argc == 2)
-	{
-		for (i = 0; i <  5 && num; i++)
-		{
-			num2 = num / coin[i];
-			change += num2;
-			num -= num2 * coin[i];
-		}
-		printf("%d\n", change);
-	}
-	else if (num < 0)
-	{
-		printf("0\n");
-	}
-	else if (argc != 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
+	}
+
+	num = atoi(argv[1]);
+
+	if (num < 1)
+		printf("0\n");
+
+	else if (argc == 2)
+	{
+		for (i = 0; i <  5 && num; i++)
+                {
+                        num2 = num / coin[i];
+                        change += num2;
+                        num -= num2 * coin[i];
+                }
+                printf("%d\n", change);
 	}
 	return (0);
 }
