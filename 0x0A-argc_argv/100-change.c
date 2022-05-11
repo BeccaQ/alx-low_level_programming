a43b1ed9d6b16d406384a4b0a99548bd04cd77e4
BeccaQ <beccamiwa@gmail.com>
2022-05-11 21:58:24 +0100

    added 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - print the minimum number of coins
+ * to make change for an amount of money.
+ * @argc: number of command line argument
+ * @argv: argument vectors
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, num, num2, change = 0;
+	int coin[] = {25, 10, 5, 2, 1};
+
+	num = atoi(argv[1]);
+
+	if (argc == 2)
+	{
+		for (i = 0; i <  5 && num; i++)
+		{
+			num2 = num / coin[i];
+			change += num2;
+			num -= num2 * coin[i];
+		}
+		printf("%d\n", change);
+	}
+	else if (num < 0)
+	{
+		printf("0\n");
+	}
+	else if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,6 @@
 0-whatsmyname.c,  prints its name, followed by a new line
 1-args.c, print the number of command line arguments passed to it
 2-args.c, print all arguments it receives
+3-mul.c, multiply two integers passed as arguments to the command line
+4-add.c, sum up all positive integers passed to the command line
+100-change.c, prints the minimum number of coins required to change an amount of money to cents
