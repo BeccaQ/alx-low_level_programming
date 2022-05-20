2b8903a7765a78df183195b352dddb2bdedcbd06
BeccaQ <beccamiwa@gmail.com>
2022-05-20 22:01:40 +0100

    fix betty style error in files

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -6,6 +6,7 @@
  * _realloc -reallocates memory block, using malloc and free
  * @old_size: int
  * @new_size: int
+ * @ptr: pointer
  * Return: a pointer to the new string
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
