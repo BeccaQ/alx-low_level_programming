0115f0109318761c5f7d77d9430d5ea2f4568484
BeccaQ <beccamiwa@gmail.com>
2022-05-20 22:21:02 +0100

    fix betty style error in files

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,18 +14,17 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *s;
 	unsigned int i;
 
-	if (!nmemb || !size)
+	if (nmemb == NULL || size == NULL)
 	{
 		return (NULL);
 	}
 
 	s = malloc(nmemb * size);
-	if (!s)
-		return (NULL);
-
-	for (i = 0; i < nmemb; i++)
-	{
-		s[i] = '\0';
-	}
-	return (s);
+	if (s == NULL)
+	return (NULL);
+		for (i = 0; i < nmemb * size; i++)
+		{
+			s[i] = '\0';
+		}
+		return (s);
 }
