74f0f4be9bea7805d106e4624e63578640b56ec9
BeccaQ <beccamiwa@gmail.com>
2022-05-30 19:59:53 +0100

    fix task 0

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,6 +14,8 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		d->name = "(nil)";
+	if (d->age == 0)
+		d->age = atoi("(nil)");
 	if (d->owner == NULL)
 		d->name = "(nil)";
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
