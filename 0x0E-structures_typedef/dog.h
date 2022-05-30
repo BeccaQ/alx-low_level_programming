67bdd1770d3aefc61a9f5fdda08276f9f55bc294
BeccaQ <beccamiwa@gmail.com>
2022-05-30 19:35:07 +0100

    fix task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,16 +11,15 @@
  * Return: nothing
  */
 
-typedef struct dog
-{
+struct dog{
 	char *name;
 	float age;
 	char *owner;
 
-} dog_tag;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
+/** dog_t *new_dog(char *name, float age, char *owner);*/
 
 #endif
