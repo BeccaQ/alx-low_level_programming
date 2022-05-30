f05ea81914a0577739862182e262f7944346328c
BeccaQ <beccamiwa@gmail.com>
2022-05-30 23:09:11 +0100

    added 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,14 +13,46 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	int len, i, j;
 
 	d = malloc(sizeof(dog_t));
-	if(!d)
+	if(!d | !name | !owner)
 	{
+		free(d);
 		return (NULL);
 	}
-	d->name = name;
+
+	for (i = 0; name[i];)
+		i++;
+	for (j = 0; owner[j];)
+		j++;
+
+	d->name = malloc(sizeof(i + 1));
+	d->owner = malloc(sizeof(j +1));
+
+	if (!(d->name) | !(d->owner))
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+		return (NULL);
+	}
+	for (len = 0; len < i; len++)
+		d->name[len] = name[len];
+	d->name[i] = '\0';
+
+	if (!(d->owner) | !(d->name))
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+	for (len = 0; len < j; len++)
+		d->owner[len] = name[len];
+	d->owner[j] = '\0';
+
 	d->age = age;
-	d->owner = owner;
+
 	return (d);
 }
